61130b6036be58b3e0e28b8d2dafbac8f4a12815
Dilsanka Sajith <Dilsanka39@gmail.com>
2025-08-17 17:03:24 +0530

    Upload Step and Steps Components Added

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,11 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { OurFileRouter } from '@/app/api/uploadthing/core';
+import { generateReactHelpers } from '@uploadthing/react';
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const { useUploadThing, uploadFiles } =
+  generateReactHelpers<OurFileRouter>();
